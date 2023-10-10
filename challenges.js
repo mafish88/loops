efe5fbe4bcc15b5434c86f62b9491afafc540425
Michael Fisher <mafish88@icloud.com>
2023-10-10 11:06:15 -0400

    Loops

---
+++ challenges.js
@@ -0,0 +1,7 @@
+const videos = ['cats', 'news', 'games', 'sports']
+// Console Log each of these videos to the screen
+// Make sure it wortks for any legnth of array.
+
+for(let i=0;i <videos.length;i ++){
+    console.log (videos[i])
+}
---
+++ loops.js
@@ -0,0 +1,28 @@
+// Say hello 3 times
+for(let i = 0; i < 3;i++){
+    console.log('hello')
+}
+
+
+
+for(let i = 0; i < 3;i++){
+    console.log('goodbye')
+}
+
+
+
+
+
+// 
+  
+
+  for(let i = 0; i < 4; i++){
+      console.log(i)
+    }
+    
+      /*
+      0
+      1
+      2
+      3
+      */
